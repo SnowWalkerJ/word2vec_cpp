7dae912c3f295875b02aaf680f288519427e044b
陈秉烜 <snowwalkerj@chenbingxuandemacbook-pro.local>
2017-08-22 21:11:35 +0800

    Initial Commit

---
+++ word2vec/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  word2vec
+//
+//  Created by 陈秉烜 on 2017/8/22.
+//  Copyright © 2017年 陈秉烜. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
